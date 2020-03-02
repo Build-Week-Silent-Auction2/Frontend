330333be561fef9fa24defe0ccde2a7c51e17922
Erik Rodriguez <eric19.21.03@gmail.com>
2020-03-02 15:43:42 -0800

    Made new components needed to push user to dash

--- silent-auction/src/utilities/axiosWithAuth.js
+++ silent-auction/src/utilities/axiosWithAuth.js
@@ -3,7 +3,7 @@ import axios from "axios";
 export const axiosWithAuth = () => {
   const token = window.localStorage.getItem("token");
   return axios.create({
-    baseURL: "https://silent-auctionbw3.herokuapp.com/",
+    baseURL: "https://silent-auctionbw3.herokuapp.com",
     headers: {
       authorization: token
     }
