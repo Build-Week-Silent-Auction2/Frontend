b6a5c4497a935cd7c8c3353ed7a9a25001b14a44
Edward Blanciak <blanciak.edward2@gmail.com>
2020-03-02 17:22:26 -0500

    added styles to the form

--- silent-auction/src/Components/Signup/SignUp.jsx
+++ silent-auction/src/Components/Signup/SignUp.jsx
@@ -7,9 +7,11 @@ import styled from "styled-components";
 
 // End of Imports
 
+// Custom Error Message
 const passError = `password must contain at least 
 1 of each uppercase/lowercase character, number, symbol`;
 
+// Form Validation
 const validationSchema = yup.object().shape({
   firstName: yup
     .string()
@@ -57,8 +59,9 @@ const validationSchema = yup.object().shape({
     .matches(/[a-z]/, passError)
     .matches(/[-+_!@#$%^&*.,?]/, passError)
 });
-// Form Styling
-const FormWrapper = styled.section`
+
+// Form Styling //
+const FormMainWrapper = styled.section`
   padding-left: 10%;
   padding-right: 10%;
   padding-top: 3%;
@@ -67,16 +70,36 @@ const FormWrapper = styled.section`
   align-items: center;
 `;
 const SignUpHeader = styled.h1`
-  color: lightblue;
-  text-decoration: underline;
+  text-align: center;
+  color: gray;
+  border-bottom: 0px;
+  width: 75%;
+  padding: 1%;
+  margin-bottom: 5%;
+`;
+const FormInsideWrapper = styled.section`
+  width: 50%;
+  padding: 2%;
+  border: 2px solid lightgray;
+  border-radius: 10px;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  background: linear-gradient(90deg, floralwhite 0%, #d9e7ff 100%);
+  box-shadow: 0px 0px 20px 10px lightgray;
+`;
+const ButtonContainer = styled.div`
+  display: flex;
+  justify-content: center;
+  margin-top: 10%;
 `;
 
-// Form Component
+// Form Component //
 const AuctionSignUp = props => {
   const useStyles = makeStyles(theme => ({
     formControl: {
-      margin: theme.spacing(1),
-      minWidth: 150
+      margin: theme.spacing(0),
+      minWidth: 225
     },
     selectEmpty: {
       marginTop: theme.spacing(2)
@@ -86,8 +109,7 @@ const AuctionSignUp = props => {
   const classes = useStyles();
 
   return (
-    <FormWrapper>
-      <SignUpHeader>Sign Up!</SignUpHeader>
+    <FormMainWrapper>
       <Formik
         initialValues={{
           firstName: "",
@@ -98,12 +120,12 @@ const AuctionSignUp = props => {
           state: "",
           zipCode: "",
           username: "",
-          password: ""
+          password: "",
+          userType: ""
         }}
         onSubmit={(data, { setSubmitting, resetForm }) => {
           console.log(data);
           setSubmitting(true);
-          //make async call
 
           props.setFormState({
             firstName: data.firstName,
@@ -114,7 +136,8 @@ const AuctionSignUp = props => {
             state: data.state,
             zipCode: data.zipCode,
             username: data.username,
-            password: data.password
+            password: data.password,
+            userType: data.userType
           });
 
           setSubmitting(false);
@@ -123,193 +146,197 @@ const AuctionSignUp = props => {
         validationSchema={validationSchema}
       >
         {({ errors, isSubmitting, isValid, values, resetForm }) => (
-          <Form>
-            <div>
-              <Field
-                id=""
-                name="firstName"
-                variant="outlined"
-                label="First Name"
-                as={TextField}
-                value={values.firstName}
-              />
-            </div>
-            <br />
-            <div>
-              <Field
-                id=""
-                name="lastName"
-                variant="outlined"
-                label="Last Name"
-                as={TextField}
-                value={values.lastName}
-              />
-            </div>
-            <br />
-            <div>
-              <Field
-                id=""
-                name="email"
-                variant="outlined"
-                label="E-mail"
-                type="email"
-                as={TextField}
-                value={values.email}
-              />
-            </div>
-            <br />
-            <div>
-              <Field
-                id=""
-                name="streetAddress"
-                variant="outlined"
-                label="Street Address"
-                type="text"
-                as={TextField}
-                value={values.streetAddress}
-              />
-            </div>
-            <br />
-            <div>
-              <Field
-                id=""
-                name="city"
-                variant="outlined"
-                label="City"
-                type="text"
-                as={TextField}
-                value={values.city}
-              />
-            </div>
-            <br />
-            <div>
-              <Field
-                as={Select}
-                variant="outlined"
-                name="state"
-                className={classes.formControl}
-              >
-                <MenuItem value="AL">Alabama</MenuItem>
-                <MenuItem value="AK">Alaska</MenuItem>
-                <MenuItem value="AZ">Arizona</MenuItem>
-                <MenuItem value="AR">Arkansas</MenuItem>
-                <MenuItem value="CA">California</MenuItem>
-                <MenuItem value="CO">Colorado</MenuItem>
-                <MenuItem value="CT">Connecticut</MenuItem>
-                <MenuItem value="DE">Delaware</MenuItem>
-                <MenuItem value="FL">Florida</MenuItem>
-                <MenuItem value="GA">Georgia</MenuItem>
-                <MenuItem value="HI">Hawaii</MenuItem>
-                <MenuItem value="ID">Idaho</MenuItem>
-                <MenuItem value="IL">Illinois</MenuItem>
-                <MenuItem value="IN">Indiana</MenuItem>
-                <MenuItem value="IA">Iowa</MenuItem>
-                <MenuItem value="KS">Kansas</MenuItem>
-                <MenuItem value="KY">Kentucky</MenuItem>
-                <MenuItem value="LA">Louisiana</MenuItem>
-                <MenuItem value="ME">Maine</MenuItem>
-                <MenuItem value="MD">Maryland</MenuItem>
-                <MenuItem value="MA">Massachusetts</MenuItem>
-                <MenuItem value="MI">Michigan</MenuItem>
-                <MenuItem value="MN">Minnesota</MenuItem>
-                <MenuItem value="MS">Mississippi</MenuItem>
-                <MenuItem value="MO">Missouri</MenuItem>
-                <MenuItem value="MT">Montana</MenuItem>
-                <MenuItem value="NE">Nebraska</MenuItem>
-                <MenuItem value="NV">Nevada</MenuItem>
-                <MenuItem value="NH">New Hampshire</MenuItem>
-                <MenuItem value="NJ">New Jersey</MenuItem>
-                <MenuItem value="NM">New Mexico</MenuItem>
-                <MenuItem value="NY">New York</MenuItem>
-                <MenuItem value="NC">North Carolina</MenuItem>
-                <MenuItem value="ND">North Dakota</MenuItem>
-                <MenuItem value="OH">Ohio</MenuItem>
-                <MenuItem value="OK">Oklahoma</MenuItem>
-                <MenuItem value="OR">Oregon</MenuItem>
-                <MenuItem value="PA">Pennsylvania</MenuItem>
-                <MenuItem value="RI">Rhode Island</MenuItem>
-                <MenuItem value="SC">South Carolina</MenuItem>
-                <MenuItem value="SD">South Dakota</MenuItem>
-                <MenuItem value="TN">Tennessee</MenuItem>
-                <MenuItem value="TX">Texas</MenuItem>
-                <MenuItem value="UT">Utah</MenuItem>
-                <MenuItem value="VT">Vermont</MenuItem>
-                <MenuItem value="VA">Virginia</MenuItem>
-                <MenuItem value="WA">Washington</MenuItem>
-                <MenuItem value="WV">West Virginia</MenuItem>
-                <MenuItem value="WI">Wisconsin</MenuItem>
-                <MenuItem value="WY">Wyoming</MenuItem>
-              </Field>
-            </div>
-            <br />
-            <div>
-              <Field
-                id=""
-                name="zipCode"
-                variant="outlined"
-                label="Zip Code"
-                type="text"
-                as={TextField}
-                value={values.zipCode}
-              />
-            </div>
-            <br />
-            <div>
-              <Field
-                id=""
-                name="username"
-                variant="outlined"
-                label="Username"
-                type="text"
-                as={TextField}
-                value={values.username}
-              />
-            </div>
-            <br />
-            <div>
-              <Field
-                id=""
-                name="password"
-                variant="outlined"
-                label="Password"
-                type="password"
-                as={TextField}
-                value={values.password}
-              />
-            </div>
-            <br />
-            <div>
-              <Field
-                as={Select}
-                variant="outlined"
-                name="userType"
-                className={classes.formControl}
-              >
-                <MenuItem value="">
-                  <em>None</em>
-                </MenuItem>
-                <MenuItem value="Buyer">Buyer</MenuItem>
-                <MenuItem value="Seller">Seller</MenuItem>
-                <MenuItem value="Both">Both</MenuItem>
-              </Field>
-            </div>
+          <FormInsideWrapper>
+            <SignUpHeader>Sign Up!</SignUpHeader>
+            <Form>
+              <div>
+                <Field
+                  id=""
+                  name="firstName"
+                  variant="outlined"
+                  label="First Name"
+                  as={TextField}
+                  value={values.firstName}
+                />
+              </div>
+              <br />
+              <div>
+                <Field
+                  id=""
+                  name="lastName"
+                  variant="outlined"
+                  label="Last Name"
+                  as={TextField}
+                  value={values.lastName}
+                />
+              </div>
+              <br />
+              <div>
+                <Field
+                  id=""
+                  name="email"
+                  variant="outlined"
+                  label="E-mail"
+                  type="email"
+                  as={TextField}
+                  value={values.email}
+                />
+              </div>
+              <br />
+              <div>
+                <Field
+                  id=""
+                  name="streetAddress"
+                  variant="outlined"
+                  label="Street Address"
+                  type="text"
+                  as={TextField}
+                  value={values.streetAddress}
+                />
+              </div>
+              <br />
+              <div>
+                <Field
+                  id=""
+                  name="city"
+                  variant="outlined"
+                  label="City"
+                  type="text"
+                  as={TextField}
+                  value={values.city}
+                />
+              </div>
+              <br />
+              <div>
+                {/* NOT DISPLAYING PLACEHOLDER */}
+                <Field
+                  as={Select}
+                  variant="outlined"
+                  name="state"
+                  className={classes.formControl}
+                >
+                  <MenuItem value="AL">Alabama</MenuItem>
+                  <MenuItem value="AK">Alaska</MenuItem>
+                  <MenuItem value="AZ">Arizona</MenuItem>
+                  <MenuItem value="AR">Arkansas</MenuItem>
+                  <MenuItem value="CA">California</MenuItem>
+                  <MenuItem value="CO">Colorado</MenuItem>
+                  <MenuItem value="CT">Connecticut</MenuItem>
+                  <MenuItem value="DE">Delaware</MenuItem>
+                  <MenuItem value="FL">Florida</MenuItem>
+                  <MenuItem value="GA">Georgia</MenuItem>
+                  <MenuItem value="HI">Hawaii</MenuItem>
+                  <MenuItem value="ID">Idaho</MenuItem>
+                  <MenuItem value="IL">Illinois</MenuItem>
+                  <MenuItem value="IN">Indiana</MenuItem>
+                  <MenuItem value="IA">Iowa</MenuItem>
+                  <MenuItem value="KS">Kansas</MenuItem>
+                  <MenuItem value="KY">Kentucky</MenuItem>
+                  <MenuItem value="LA">Louisiana</MenuItem>
+                  <MenuItem value="ME">Maine</MenuItem>
+                  <MenuItem value="MD">Maryland</MenuItem>
+                  <MenuItem value="MA">Massachusetts</MenuItem>
+                  <MenuItem value="MI">Michigan</MenuItem>
+                  <MenuItem value="MN">Minnesota</MenuItem>
+                  <MenuItem value="MS">Mississippi</MenuItem>
+                  <MenuItem value="MO">Missouri</MenuItem>
+                  <MenuItem value="MT">Montana</MenuItem>
+                  <MenuItem value="NE">Nebraska</MenuItem>
+                  <MenuItem value="NV">Nevada</MenuItem>
+                  <MenuItem value="NH">New Hampshire</MenuItem>
+                  <MenuItem value="NJ">New Jersey</MenuItem>
+                  <MenuItem value="NM">New Mexico</MenuItem>
+                  <MenuItem value="NY">New York</MenuItem>
+                  <MenuItem value="NC">North Carolina</MenuItem>
+                  <MenuItem value="ND">North Dakota</MenuItem>
+                  <MenuItem value="OH">Ohio</MenuItem>
+                  <MenuItem value="OK">Oklahoma</MenuItem>
+                  <MenuItem value="OR">Oregon</MenuItem>
+                  <MenuItem value="PA">Pennsylvania</MenuItem>
+                  <MenuItem value="RI">Rhode Island</MenuItem>
+                  <MenuItem value="SC">South Carolina</MenuItem>
+                  <MenuItem value="SD">South Dakota</MenuItem>
+                  <MenuItem value="TN">Tennessee</MenuItem>
+                  <MenuItem value="TX">Texas</MenuItem>
+                  <MenuItem value="UT">Utah</MenuItem>
+                  <MenuItem value="VT">Vermont</MenuItem>
+                  <MenuItem value="VA">Virginia</MenuItem>
+                  <MenuItem value="WA">Washington</MenuItem>
+                  <MenuItem value="WV">West Virginia</MenuItem>
+                  <MenuItem value="WI">Wisconsin</MenuItem>
+                  <MenuItem value="WY">Wyoming</MenuItem>
+                </Field>
+              </div>
+              <br />
+              <div>
+                <Field
+                  id=""
+                  name="zipCode"
+                  variant="outlined"
+                  label="Zip Code"
+                  type="text"
+                  as={TextField}
+                  value={values.zipCode}
+                />
+              </div>
+              <br />
+              <div>
+                <Field
+                  id=""
+                  name="username"
+                  variant="outlined"
+                  label="Username"
+                  type="text"
+                  as={TextField}
+                  value={values.username}
+                />
+              </div>
+              <br />
+              <div>
+                <Field
+                  id=""
+                  name="password"
+                  variant="outlined"
+                  label="Password"
+                  type="password"
+                  as={TextField}
+                  value={values.password}
+                />
+              </div>
+              <br />
+              <div>
+                {/* NOT DISPLAYING PLACEHOLDER */}
+                <Field
+                  as={Select}
+                  variant="outlined"
+                  name="userType"
+                  placeholder="User"
+                  label="User"
+                  className={classes.formControl}
+                  value={values.userType}
+                >
+                  <MenuItem value="Buyer">Buyer</MenuItem>
+                  <MenuItem value="Seller">Seller</MenuItem>
+                </Field>
+              </div>
 
-            <div>
-              <Button
-                type="submit"
-                variant="contained"
-                disabled={!isValid || isSubmitting || false}
-              >
-                Submit
-              </Button>
-            </div>
+              <ButtonContainer>
+                <Button
+                  type="submit"
+                  variant="contained"
+                  disabled={!isValid || isSubmitting || false}
+                >
+                  Submit
+                </Button>
+              </ButtonContainer>
 
-            <pre>{JSON.stringify(values, null, 2)}</pre>
-            <pre>{JSON.stringify(errors, null, 2)}</pre>
-          </Form>
+              {/* <pre>{JSON.stringify(values, null, 2)}</pre>
+              <pre>{JSON.stringify(errors, null, 2)}</pre> */}
+            </Form>
+          </FormInsideWrapper>
         )}
       </Formik>
-    </FormWrapper>
+    </FormMainWrapper>
   );
 };
 
