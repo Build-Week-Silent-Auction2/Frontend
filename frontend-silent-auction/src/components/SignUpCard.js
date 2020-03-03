63040806345dc6b2344baa9340f7c899a412ccf0
Mina-Ramzey <mramz1337@gmail.com>
2020-03-02 19:13:15 -0500

    Login/Signup still need to connect

---
+++ frontend-silent-auction/src/components/SignUpCard.js
@@ -0,0 +1,15 @@
+import React from "react";
+import SignUp  from "./components/SignUp";
+
+
+import React from 'react'
+
+const SignUpCard = () => {
+    return (
+        <div>
+            <SignUp/>
+        </div>
+    )
+}
+
+export default SignUpCard
