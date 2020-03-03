6819a81b8162c07b725ffdd83b922355198ce64c
Edward Blanciak <blanciak.edward2@gmail.com>
2020-03-02 23:31:55 -0500

    added some random filler to the home page.. temporary

--- silent-auction/src/Components/Home/HomePage.jsx
+++ silent-auction/src/Components/Home/HomePage.jsx
@@ -1,9 +1,17 @@
 import React from "react";
+import { BrowserRouter as Router, Route, Link } from "react-router-dom";
 
 const HomePage = () => {
   return (
     <div className="home-page-wrapper">
-      <p>Home Page Here</p>
+      <h1>Home Page Here</h1>
+      <p>Welcome to Silent Auction</p>
+      <br />
+      <span>Sign up </span>
+      <Link to="/signup">here!</Link>
+      <br />
+      <span>Login </span>
+      <Link to="/login">here!</Link>
     </div>
   );
 };
