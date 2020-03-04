c746ce337004304f04f5f267d4861855b6475795
Edward Blanciak <blanciak.edward2@gmail.com>
2020-03-04 15:36:57 -0500

    idk

--- silent-auction/src/Components/BidderDash.js
+++ silent-auction/src/Components/BidderDash.js
@@ -1,7 +1,56 @@
 import React from "react";
 import { useHistory } from "react-router-dom";
+import { makeStyles } from "@material-ui/core/styles";
+import clsx from "clsx";
+import Card from "@material-ui/core/Card";
+import CardHeader from "@material-ui/core/CardHeader";
+import CardMedia from "@material-ui/core/CardMedia";
+import CardContent from "@material-ui/core/CardContent";
+import CardActions from "@material-ui/core/CardActions";
+import Collapse from "@material-ui/core/Collapse";
+import Avatar from "@material-ui/core/Avatar";
+import IconButton from "@material-ui/core/IconButton";
+import Typography from "@material-ui/core/Typography";
+import { red } from "@material-ui/core/colors";
+import FavoriteIcon from "@material-ui/icons/Favorite";
+import ShareIcon from "@material-ui/icons/Share";
+import ExpandMoreIcon from "@material-ui/icons/ExpandMore";
+import MoreVertIcon from "@material-ui/icons/MoreVert";
+// Material UI Styles //
+const useStyles = makeStyles(theme => ({
+  root: {
+    maxWidth: 345
+  },
+  media: {
+    height: 0,
+    paddingTop: "56.25%" // 16:9
+  },
+  expand: {
+    transform: "rotate(0deg)",
+    marginLeft: "auto",
+    transition: theme.transitions.create("transform", {
+      duration: theme.transitions.duration.shortest
+    })
+  },
+  expandOpen: {
+    transform: "rotate(180deg)"
+  },
+  avatar: {
+    backgroundColor: red[500]
+  }
+}));
 
-const BidderDash = (props) => {
+// Dashboard Component //
+const BidderDash = props => {
+  // Brings in styles //
+  const classes = useStyles();
+  // Material UI required state //
+  const [expanded, setExpanded] = React.useState(false);
+  // Material UI required handler //
+  const handleExpandClick = () => {
+    setExpanded(!expanded);
+  };
+
   const history = useHistory();
   const signOut = () => {
     window.localStorage.removeItem("token");
@@ -10,8 +59,6 @@ const BidderDash = (props) => {
   return (
     <div>
       <button onClick={signOut}>Sign Out</button>
-      <p>bidderDash.js</p>
-
       <button onClick={() => history.push("/shop/")}>Go To Auctions</button>
     </div>
   );
